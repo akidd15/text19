9165354d186b57127cda914c79f0ab2cbd3061c8
Allison Farris <allison.perry98@gmail.com>
2023-11-20 01:14:59 -0700

    configured database

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,3 +1,4 @@
+// Import necessary Workbox modules
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
@@ -5,8 +6,10 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// Precache static assets
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Cache strategy for pages
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -14,17 +17,31 @@ const pageCache = new CacheFirst({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // Cache pages for 30 days
     }),
   ],
 });
 
+// Warm the cache for specific URLs
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
+// Register the route for navigating pages
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
+// Register route for caching assets
 // TODO: Implement asset caching
-registerRoute();
+// For example, CacheFirst for images with expiration
+registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60, // Cache images for 7 days
+      }),
+    ],
+  })
+);
--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+
+const jate = await openDB('jate', 1);
+const tx = jate.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+const request = store.add({ text: content });
+const result = await request;
+console.log('Data added to the database', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { 
+  console.error('getDb not implemented');
 
+const jate = await openDB('jast', 1);
+const tx = jate.transaction('jast', 'readonly');
+const store = tx.objectStore('jate');
+const request = store.getAll();
+
+const result = await request;
+console.log('result.value', result);
+return result;
+
+}
 initdb();
+// npm install webpack
+// npm i
+// npm run start
